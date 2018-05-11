61138e9241b10b193757c689b20987aec6e0fc6d
Cris Wilson <cris.wilson77@gmail.com>
2018-05-11 09:12:39 +0100

    Frist

---
+++ app/factories/messagesFactory.js
@@ -0,0 +1,16 @@
+angular.module('myApp')
+    .Factory('retailerFactory', function ($http) {
+        var endPoint = '/_vti_bin/Brightstarr.EI.Intranet/Retailer/Retailer.svc/'
+        
+        function getAccounts(userId) {
+            return $http.get(endPoint + 'GetPubAccounts?userId=' + userId);
+        }
+
+        function getDashboardMessages(userId, pageNumber, pageSize) {
+            return $http.get(endpoint + 'GetDashboardMessages?userId=' + userId + '&pageNumber=' + pageNumber + '&take=' + messagePageSize + '&n=0');
+        }
+
+        function deleteDashboardMessages(userId, messageId) {
+            return $http.post(endPoint + 'DeleteDashboardMessage', { "userId": userId, "messageId": messageId });
+        }
+    });
